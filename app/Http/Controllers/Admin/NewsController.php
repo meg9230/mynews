19f873615bf4740528453fd0bbee2adb1578863f
Megumi Kutsuzawa <mgm.ktzw07@gmail.com>
2020-09-11 05:51:52 +0000

    PHP/Laravel_Heroku

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use App\News;
 use App\History;
 use Carbon\Carbon;
+use Storage;
 
 // ニュース投稿に関するメソッド
 class NewsController extends Controller
@@ -28,8 +29,8 @@ class NewsController extends Controller
         
         // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
         if(isset($form['image'])) {
-            $path = $request->file('image')->store('public/image');
-            $news->image_path = basename($path);
+            $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+            $news->image_path = Storage::disk('s3')->url($path);
         } else {
             $news->image_path = null;
         }
@@ -74,8 +75,8 @@ class NewsController extends Controller
         $news_form = $request->all();
         
         if(isset($news_form['image'])) {
-            $path = $request->file('image')->store('public/image');
-            $news->image_path = basename($path);
+            $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+            $news->image_path = Storage::disk('s3')->url($path);
             unset($news_form['image']);
         } elseif(isset($request->remove)) {
             $news->image_path = null;
